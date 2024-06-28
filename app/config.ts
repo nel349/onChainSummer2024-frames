a996d53853a3d507c07811f235863b3c14ec5dbb
Norman Lopez <noell.lpz@gmail.com>
2024-06-28 03:53:36 -0700

    Update config to use environment variable for production URL

--- app/config.ts
+++ app/config.ts
@@ -2,5 +2,5 @@
 
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL =
-  process.env.NEXT_TESTING_MODE == 'LOCAL' ? 'http://localhost:3000' : ' https://d857-2600-100f-a101-9e22-598e-623f-f200-2e08.ngrok-free.app';
-export const BUY_MY_COFFEE_CONTRACT_ADDR = '0xcD3D5E4E498BAb2e0832257569c3Fd4AE439dD6f';
+  process.env.NEXT_PUBLIC_TESTING_MODE == 'LOCAL' ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_PROD_URL;
+export const BUY_MY_COFFEE_CONTRACT_ADDR = '0xcD3D5E4E498BAb2e08322257569c3Fd4AE439dD6f';
